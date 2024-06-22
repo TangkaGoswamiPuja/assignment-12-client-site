064d472ce2c2df560f368686b68e45fcd69ec33e
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-06-22 23:38:05 +0600

    last

--- src/Hooks/useTests.jsx
+++ src/Hooks/useTests.jsx
@@ -1,31 +1,16 @@
-// import { useEffect, useState } from "react";
-
 import { useQuery } from "@tanstack/react-query";
 import useAxiosOpen from "./useAxiosOpen";
-
 const useTests = () => {
-    // const [test,setTest]=useState([]);
-    // const [loading,setLoading] = useState(true);
-    // useEffect(()=>{
-    //     fetch('https://doc-server-site.vercel.app/alltest')
-    //     .then(res=>res.json())
-    //     .then(data=>{
-    //          setTest(data);
-    //         setLoading(false);
-    //     })
-
-    // },[])
-    const axiosOpen = useAxiosOpen();
-    // const {user }= useAuth();
-        const { refetch, data: test = [] ,isPending:loading} = useQuery({
-        queryKey:['test'],
-        queryFn:async ()=>{
-            const res=await axiosOpen.get('/alltest');
+const axiosOpen = useAxiosOpen();
+    const { refetch, data: test = [], isPending: loading } = useQuery({
+        queryKey: ['test'],
+        queryFn: async () => {
+            const res = await axiosOpen.get('/alltest');
             return res.data;
         }
 
-    }) 
-    return [test , loading ,refetch] 
+    })
+    return [test, loading, refetch]
 }
 
 export default useTests;
